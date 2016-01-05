e7a4ca8fec415aea5529266b1c2db5159bbb770c
Houghton <dhoughton@daniel-houghton-2.local>
2016-01-05 14:30:17 -0500

    first test commit

--- pechakuchavideo.py
+++ pechakuchavideo.py
@@ -1,6 +1,7 @@
 # Videographic Criticism using Chance Operations
 # by Mark Sample http://www.samplereality.com
 # based on an idea by Jason Mittell http://justtv.wordpress.com
+# updated and made more generalized by Daniel Houghton 2015-16
 
 # Inspired by the Pecha Kucha format, this program assembles
 # ten random 6-second clips from a single video
@@ -12,14 +13,23 @@
 # random picks the random start and stop times
 from moviepy.editor import *
 import random
+import sys
+
+if ( len(sys.argv) != 2):
+	print("Arguments expected in the following format: pechakuchavideo.py source_video")
+	exit()
+
+
+source_video_name = sys.argv[1]
+source_video = VideoFileClip(source_video_name)
 
 # Random Audio Clip
 # Next we select a random audio clip from the source video
 # The "begin" variable selects a random timecode, in seconds
 # (0,2754) indicates the length of the video, in seconds, minus 60 seconds
-begin = random.randint(0,2754)
-snd = AudioFileClip('madmens7e1.mp4').subclip(begin,begin+60)
-snd.write_audiofile('audiotrack.mp3')
+begin = random.randint(0, int(source_video.duration - 60))
+snd = AudioFileClip(source_video_name).subclip(begin,begin+60)
+snd.write_audiofile(source_video_name.split(".")[0] + '_audiotrack.mp3')
 
 # Random Video Clips
 # Next we select the 10 random clips, 6 seconds each
@@ -29,27 +39,21 @@ snd.write_audiofile('audiotrack.mp3')
 # The "end" variable is begin+6, making each clip 6 seconds long
 # clip.volumex removes the audio from each clip
 # We also save each new clip with a distinct file name
+clip_list = []
+
 for i in range(0,10):
-    begin = random.randint(0,2808)
+    begin = random.randint(0,int(source_video.duration - 6))
     end = begin + 6
-    clip = VideoFileClip("madmens7e1.mp4").subclip(begin, end)
+    clip = source_video.subclip(begin, end)
     clip = clip.volumex(0)
-    clip.write_videofile("madmenclip"+str(i)+".mp4")
+    clip.write_videofile(source_video_name.split(".")[0]+ "_" + str(i) + ".mp4")
+    clip_list.append(clip)
 
 # Now we assign names to each saved clip
-clip0 = VideoFileClip('madmenclip0.mp4')
-clip1 = VideoFileClip('madmenclip1.mp4')
-clip2 = VideoFileClip('madmenclip2.mp4')
-clip3 = VideoFileClip('madmenclip3.mp4')
-clip4 = VideoFileClip('madmenclip4.mp4')
-clip5 = VideoFileClip('madmenclip5.mp4')
-clip6 = VideoFileClip('madmenclip6.mp4')
-clip7 = VideoFileClip('madmenclip7.mp4')
-clip8 = VideoFileClip('madmenclip8.mp4')
-clip9 = VideoFileClip('madmenclip9.mp4')
+
 
 # We assemble all the clips into something called "final_clip"
-final_clip = concatenate_videoclips([clip0,clip1,clip2,clip3,clip4,clip5,clip6,clip7,clip8,clip9])
+final_clip = concatenate_videoclips(clip_list)
 
 # Finally, save the assembled final_clip, using the random audio file created above
-final_clip.write_videofile("pechakucha.mp4",audio="audiotrack.mp3")
+final_clip.write_videofile(source_video_name.split(".")[0] + "_pechakucha.mp4",audio=source_video_name.split(".")[0] + '_audiotrack.mp3')
